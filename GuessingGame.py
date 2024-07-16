e002538eddd37a2fb8018c976fe5b90438e16280
kaleycode <readingkale@gmail.com>
2024-07-16 15:31:40 -0700

    Update GuessingGame.py

--- GuessingGame.py
+++ GuessingGame.py
@@ -5,23 +5,26 @@ print()
 
 correctNumber = 66
 attempt = 1
-
 while True:
-  userguess = int(input("Pick a number between 1 and 100. "))
+  userguess=int(input("Type in a number between 1 and 100: "))
   if userguess < 0:
-    print("Nope! It's not a negative number!")
+    print("Nope! It's not a negative number. Try again!")
     attempt +=1
     continue
-  if userguess < correctNumber:
-    print("Nope, too low!")
-    attempt += 1
+  elif userguess < correctNumber:
+    print("Nope! That's too low, try again!")
+    attempt +=1
+    continue
   elif userguess > correctNumber:
-    print("That number is too high. Try again!")
-    attempt += 1
+    print("Nope, too high!")
+    attempt +=1
     continue
   elif userguess == correctNumber:
-    print("It took you", attempt, "attempt(s) to get the correct answer.")
-    print("YOU WIN")
+    print("YOU WIN!")
+    print("It took you", attempt, "attempt(s) to get the correct answer")
     break
   else:
-    print("That is not a number you can use.")
+    print("Pick a number between 1 and 100. ")
+    attempt +=1
+    continue
+  
